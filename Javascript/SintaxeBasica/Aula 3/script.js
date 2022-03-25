52638d71563a76f41cfea551c781b6b835231db7
Andre Oliveira <a.hardware@hotmail.com>
2022-03-25 00:48:24 -0300

    Javascript DIO course. First Part.

--- Javascript/SintaxeBasica/Aula 3/script.js
+++ Javascript/SintaxeBasica/Aula 3/script.js
@@ -32,4 +32,15 @@ console.log(novoArray);
 ///////// Objetos ///////
 
 
-let object = {string: 'String', Number: 1, array: ["array"]}
+let object = {string: 'String', Number: 1, array: ["array"], objectInterno:{objectInterno: 'objeto interno'}};
+
+console.log(object.objectInterno);
+
+var string = object.string;
+console.log(string);
+
+var arrayInterno = object.array;
+console.log(arrayInterno);
+
+var {string, boolean, objectInterno} = object;
+console.log(string, boolean, objectInterno);
