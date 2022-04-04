457d21ad9ae64b0cfb565c163b060793c55afef4
Andre Oliveira <a.hardware@hotmail.com>
2022-04-03 22:44:08 -0300

    Continue the Javascript learnings

---
+++ Javascript/AulaPratica/SubsNumPares.js
@@ -0,0 +1,18 @@
+function subsPares(array){
+    if(!array.length) return -1;
+
+    for(let i =0; i < array.length; i++){
+        if(array[i] === 0){
+        console.log("Array já é zero!")
+        }else if (array[i] % 2 === 0){
+            console.log(`Substituindo ${array[i]} por 0...`);
+            array[i] = 0;
+        }
+
+    }
+    return array;
+}
+
+let array1 = [1, 3, 4, 6, 80, 33, 23, 90];
+console.log(subsPares(array1)); 
+console.log(subsPares(array1));
---
+++ Javascript/AulaPratica/verificarPalindromo.js
@@ -0,0 +1,25 @@
+//Solução1
+function verificaPalindromo(string){
+    if(!string) return "String Inexistente!";
+
+    return string.split("").reverse().join("") === string;
+}
+
+console.log(verificaPalindromo("ovo"));
+
+//Solução2
+
+function verificaPalindromo2(string){
+    if(!string) return "String Inexistente";
+
+    for(let i = 0; i < string.length / 2 ; i++){
+        if(string[i] !== string[string.length - 1 - i]){
+            return false;
+        }
+
+        return true;
+
+    }
+
+}
+console.log(verificaPalindromo2("gato"));
